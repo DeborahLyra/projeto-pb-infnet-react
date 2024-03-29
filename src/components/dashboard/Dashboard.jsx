8046ffcd44e5b573d8767aaaa8516e7cd2478aa9
Babi Costa <babiicoosta2@gmail.com>
2024-03-29 19:38:28 -0300

    Implementando listagem de topicos

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,48 +1,100 @@
-import styles from './Dashboard.module.css'
-import { Comment } from '../comment/Comment'
-import { Avatar } from '../avatar/Avatar'
+import styles from "./Dashboard.module.css";
+import { Comment } from "../comment/Comment";
+import { Avatar } from "../avatar/Avatar";
+import { Trash, ThumbsUp, ArrowCircleDown } from 'phosphor-react'
+import { useState } from "react";
 
 export function Dashboard({ author, content }) {
+  
+  const data = [
+    {
+      nome: "Topico 1",
+      comentarios: false,
+    },
+    {
+      nome: "Topico 2",
+      comentarios: true,
+    },
+  ];
+
+  const [topicos, setTopicos] = useState(data);
+
+  const atualizarComentarios = (index) => {
+    const novosTopicos = [...topicos];
+    novosTopicos[index].comentarios = !novosTopicos[index].comentarios;
+    setTopicos(novosTopicos);
+  };
+
   return (
-    <article className={styles.posts}>
-      <header>
-        <div className={styles.author}>
-          <Avatar  src="https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D" alt="" />
-          <div className={styles.authorInfo}>
-            <strong>The Worried Pug</strong>
-            <span>Web Developer</span>
-          </div>
-        </div>
-        <time title='05 de março as 12h' dateTime="2024-03-05 12:00:00">Publicado há 1 hora</time>
-      </header>
+    <>
+      {topicos.map((topico, index) => {
+        return (
+          <article className={styles.posts}>
+            <header>
+              <div className={styles.author}>
+                <Avatar
+                  src="https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D"
+                  alt=""
+                />
+                <div className={styles.authorInfo}>
+                  <strong>The Worried Pug</strong>
+                  <span>Web Developer</span>
+                </div>
+              </div>
+              <time title="05 de março as 12h" dateTime="2024-03-05 12:00:00">
+                Publicado há 1 hora
+              </time>
+            </header>
 
-      <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 </p>
-        <p>👉<a href="#"> jane.design/doctorcare </a></p>
-        <p>
-          <a href="#">#novoprojeto</a>{' '}
-          <a href="#">#nlw</a>{' '}
-          <a href="#">#rocketseat</a>{' '}
-        </p>
-      </div>
+            <div className={styles.content}>
+                  <p>Fala galeraa 👋</p>
+                  <p>
+                    {" "}
+                    Acabei de subir mais um projeto no meu portifa. É um projeto
+                    que fiz no NLW Return, evento da Rocketseat. O nome do
+                    projeto é DoctorCare 🚀{" "}
+                  </p>
+                  <p>
+                    👉<a href="#"> jane.design/doctorcare </a>
+                  </p>
+                  <p>
+                    <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+                    <a href="#">#rocketseat</a>{" "}
+                  </p>
+                  <div className="flex mt-3 gap-2">
+                    <button onClick={() => atualizarComentarios(index)}>
+                      <ArrowCircleDown size={24} />
+                    </button>
+                    <button>
+                        <ThumbsUp size={24} />
+                    </button>
+                    <button title='trash'>
+                        <Trash size={24} />
+                    </button>
+                  </div>
+            </div>
 
-      <form className={styles.commentForm}>
-        <strong>Deixe seu comentário</strong>
-        <textarea
-          placeholder='deixe um comentário'
-        />
-        <footer>
-          <button type='submit'>Publicar</button>
-        </footer>
-        {/* precisou criar esse footer para alterar o bnt no css */}
-      </form>
+            {topico.comentarios ? (
+              <>
+                <form className={styles.commentForm}>
+                  <strong>Deixe seu comentário</strong>
+                  <textarea placeholder="deixe um comentário" />
+                  <footer>
+                    <button type="submit">Publicar</button>
+                  </footer>
+                  {/* precisou criar esse footer para alterar o bnt no css */}
+                </form>
 
-      <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
-        <Comment/>
-      </div>
-    </article>
-  )
+                <div className={styles.commentList}>
+                  <Comment />
+                  <Comment />
+                  <Comment />
+                </div>
+              </>
+            ) : null}
+          </article>
+        );
+      })}
+    </>
+  );
 }
