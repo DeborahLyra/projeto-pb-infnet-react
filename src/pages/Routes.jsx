86425cd582025e9eb131d1f06224c821b52f3d84
Deborah Lyra <deborahp.lyra@gmail.com>
2024-03-29 17:00:32 -0300

    login certo

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DashboardPage from './DashboardPage';
 import RankingPage from './RankingPage';
 import SignInPage from './SignInPage';
-import Login from './Login';
+import LoginPage from './LoginPage';
 
 
 const AppRoutes  = () => (
@@ -12,7 +12,7 @@ const AppRoutes  = () => (
       <Route exact path="/dashboard" element={<DashboardPage/>} />
       <Route exact path="/ranking" element={<RankingPage/>} />
       <Route exact path="/signIn" element={<SignInPage/>} />
-      <Route exact path="/login" element={<Login/>} />
+      <Route exact path="/login" element={<LoginPage/>} />
     </Routes>
   </BrowserRouter>
 );
